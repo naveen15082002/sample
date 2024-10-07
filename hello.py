9832da7ad3f161b5e308e3d2cbbd20368553804a
Aswin S <105501058+aswinjak@users.noreply.github.com>
2024-10-07 09:55:31 +0530

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to home page"
+
+
+@app.route('/city')
+def city():
+    return "Welcome to Chennai"
+
+
+@app.route('/org')
+def org():
+    return "Welcome to RNTBCI"
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
